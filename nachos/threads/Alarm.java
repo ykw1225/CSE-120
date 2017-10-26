b6b8da929c990c3b52639b17ff2f4bd3d30227ba
Chao Huang <chh115@its-cseb260-09.ucsd.edu>
2017-10-25 19:39:21 -0700

    oct 25

--- nachos/threads/Alarm.java
+++ nachos/threads/Alarm.java
@@ -12,7 +12,7 @@ public class Alarm {
     /**
      * Allocate a new Alarm. Set the machine's timer interrupt handler to this
      * alarm's callback.
-     *
+     * 
      * <p>
      * <b>Note</b>: Nachos will not function correctly with more than one alarm.
      */
@@ -61,7 +61,7 @@ public class Alarm {
      * should be run.
      */
     public void timerInterrupt() {
-        Machine.interrupt().disable();
+        boolean status = Machine.interrupt().disable();
         if(!threadQ.isEmpty()){
             AKThread toCheck = threadQ.peek();
             if(toCheck.wake_time <= Machine.timer().getTime()){
@@ -69,7 +69,7 @@ public class Alarm {
                 threadQ.poll();
             }
         }
-        Machine.interrupt().enable();
+        Machine.interrupt().restore(status);
         KThread.currentThread().yield();
     }
 
@@ -77,33 +77,29 @@ public class Alarm {
      * Put the current thread to sleep for at least <i>x</i> ticks, waking it up
      * in the timer interrupt handler. The thread must be woken up (placed in
      * the scheduler ready set) during the first timer interrupt where
-     *
+     * 
      * <p>
      * <blockquote> (current time) >= (WaitUntil called time)+(x) </blockquote>
-     *
+     * 
      * @param x the minimum number of clock ticks to wait.
-     *
+     * 
      * @see nachos.machine.Timer#getTime()
      */
     public void waitUntil(long x) {
         // for now, cheat just to get something working (busy waiting is bad)
-        Machine.interrupt().disable();
-
+        boolean status = Machine.interrupt().disable();
         if(x <= 0){
             return;
         }
-
         long wakeTime = Machine.timer().getTime() + x;
-
         if(wakeTime > Machine.timer().getTime()){
             AKThread toWait = new AKThread(KThread.currentThread(), wakeTime);
             if(threadQ.size() < 5000){
               threadQ.add(toWait);
-              KThread.currentThread().sleep();
+              KThread.sleep();
             }
         }
-
-        Machine.interrupt().enable();
+        Machine.interrupt().restore(status);
     }
 
     // Add Alarm testing code to the Alarm class
@@ -118,60 +114,39 @@ public class Alarm {
             t1 = Machine.timer().getTime();
             System.out.println ("alarmTest1: waited for " + (t1 - t0) + " ticks");
         }
-
-    System.out.println("--Finish alarm test 1---");
     }
-
-
      private static class alarmTest2 implements Runnable {
          int durations[] = {1000, 10*1000, 100*1000};
             long t0, t1;
-            String name;
-                alarmTest2(String input){
+            int name;
+            alarmTest2(int input){
                 name = input;
             }
-
-            public void run() {
-                 for (int d : durations) {
+              public void run() {
+                  for (int d : durations) {
                     t0 = Machine.timer().getTime();
                     ThreadedKernel.alarm.waitUntil (d);
                     t1 = Machine.timer().getTime();
-                    System.out.println("Time to wait: " + d);
-                    System.out.println("Time before wait: " + t0);
-                    System.out.println("Time after wait: " + t1);
-                    System.out.println ("Thread: " + name + " : waited for " + (t1 - t0) + " ticks");
-            System.out.println("-----------------");
+                    System.out.println("d" + d);
+                    System.out.println("t0: " + t0);
+                    System.out.println("t1: " + t1);
+                    System.out.println (name + "alarmTest1: waited for " + (t1 - t0) + " ticks");
                 }
-            }
-      }
+              }
+          }
     
 
     // Implement more test methods here ...
 
     // Invoke Alarm.selfTest() from ThreadedKernel.selfTest()
     public static void selfTest() {
-
-    	// Invoke your other test methods here ...
-   	System.out.println("-----------Starting Alarm Test 1-------------");
         alarmTest1();
-   	System.out.println("-----------Starting Alarm Test 2-------------");
-        alarmTest2 test2 = new alarmTest2("00");
+        alarmTest2 test2 = new alarmTest2(999);
         new KThread(test2).fork();
-
-        new alarmTest2("11").run();
-
-    //threadQ.peek().thread.ready();
-	ThreadedKernel.alarm.waitUntil(20000);
-	
-	System.out.println("-----------End All Alarm Tests-------------\n");
-        
+        new alarmTest2(11).run();
+        // Invoke your other test methods here ...
     }
 }
-
-
-
-
-
 
 
 
